4238a6fc20fbc231b8329db52627645aff298782
ShirelB <shirel1b@gmail.com>
2018-07-18 16:14:51 +0300

    another rearrange the files of project

--- routing/components/About/aboutController.js
+++
@@ -1,7 +0,0 @@
-angular.module('pointsOfInterestApp')
-.controller('aboutController', ['$scope', function($scope) {
-    $scope.count = 0;
-    $scope.myFunc = function() {
-      $scope.count++;
-    };
-  }]);
